2ab329600333b756ba3af53051d2256d82f5269e
xjj <xjjakm@outlook.com>
2026-05-09 21:16:04 +0800

    26.1.2 update

--- src/main/java/com/yalu/addon/TranslateAddon.java
+++ src/main/java/com/yalu/addon/TranslateAddon.java
@@ -1,6 +1,5 @@
 package com.yalu.addon;
 
-import com.mojang.logging.LogUtils;
 import com.yalu.addon.modules.AboutThisPlugin;
 import meteordevelopment.meteorclient.MeteorClient;
 import meteordevelopment.meteorclient.addons.GithubRepo;
@@ -8,11 +7,9 @@ import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.client.Minecraft;
-import org.slf4j.Logger;
 
 public class TranslateAddon extends MeteorAddon {
     public static final String VERSION = "1.0.2";
-    public static final Logger LOG = LogUtils.getLogger();
     public static final Category CATEGORY = new Category("I18n");
     public static final Minecraft MC = MeteorClient.mc;
     public static final Translator TRANSLATOR = new Translator();
