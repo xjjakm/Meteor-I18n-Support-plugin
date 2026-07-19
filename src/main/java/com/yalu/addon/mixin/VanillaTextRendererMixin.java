20013e9306a3a95171bd462c34328290cab68552
xjj <xjjakm@outlook.com>
2026-07-19 21:43:07 +0800

    bump version

--- src/main/java/com/yalu/addon/mixin/VanillaTextRendererMixin.java
+++ src/main/java/com/yalu/addon/mixin/VanillaTextRendererMixin.java
@@ -74,6 +74,7 @@ public abstract class VanillaTextRendererMixin {
         return (x / scale + width - 1) * scale;
     }
 
+    @Overwrite
     public void end(PoseStack matrices) {
         if (!building) throw new RuntimeException("VanillaTextRenderer.end() called without calling begin()");
 
