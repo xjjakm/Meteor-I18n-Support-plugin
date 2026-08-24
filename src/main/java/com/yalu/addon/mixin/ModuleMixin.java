cac19e09636e9cc830b7dc7eb49ec6dff774499a
dingzhen-vape <327112257@qq.com>
2026-08-25 00:07:14 +0800

    翻译键支持多插件前缀,补充 Meteor+ 与 Catppuccin 中文翻译

--- src/main/java/com/yalu/addon/mixin/ModuleMixin.java
+++ src/main/java/com/yalu/addon/mixin/ModuleMixin.java
@@ -3,6 +3,7 @@ package com.yalu.addon.mixin;
 import com.yalu.addon.TranslateAddon;
 import com.yalu.addon.Translator;
 import meteordevelopment.meteorclient.MeteorClient;
+import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.utils.Utils;
 import org.spongepowered.asm.mixin.*;
@@ -28,13 +29,22 @@ public abstract class ModuleMixin {
     @Shadow
     @Final
     public String description;
+    @Shadow
+    @Final
+    public MeteorAddon addon;
     @Unique
     public String name;
     @Inject(method = "<init>*", at = @At("RETURN"))
     public void onInit(CallbackInfo ci){
         TRANSLATOR.reload(MC.getResourceManager());
-        String ModuleKey = "Module.Meteor." + this.name;
-        String DescriptionKey = "Module.Meteor." + this.name + ".Description";
+
+        String PackageName = this.addon.name.replace(" ", "-");
+        if (PackageName.equals("Meteor-Client")){//历史遗留问题,我不想改语言文件
+            PackageName = "Meteor";
+        }
+
+        String ModuleKey = "Module." + PackageName + "." + this.name;
+        String DescriptionKey = "Module." + PackageName + "." + this.name + ".Description";
         this.title = TRANSLATOR.Translate(ModuleKey, this.name);
         this.description = TRANSLATOR.Translate(DescriptionKey,this.description);
     }
