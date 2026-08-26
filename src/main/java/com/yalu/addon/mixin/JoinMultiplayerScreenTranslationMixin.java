a0a917a1903794ccf018a6ce7ed7fec128e43ce9
xjj <xjjakm@outlook.com>
2026-08-26 21:32:57 +0800

    删除多余的代码

--- src/main/java/com/yalu/addon/mixin/JoinMultiplayerScreenTranslationMixin.java
+++ src/main/java/com/yalu/addon/mixin/JoinMultiplayerScreenTranslationMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = JoinMultiplayerScreen.class, priority = 1100)
@@ -30,14 +29,5 @@ public abstract class JoinMultiplayerScreenTranslationMixin extends Screen {
                 }
             }
         }
-    }
-
-    @ModifyArg(
-        method = "extractRenderState",
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphicsExtractor;text(Lnet/minecraft/client/gui/Font;Ljava/lang/String;III)V"),
-        index = 1
-    )
-    private String onTextRender(String str) {
-        return TextReplacement.replace(str);
     }
 }
